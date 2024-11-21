8d55b7f32d58ff31f849315053fd274fb6765884
Edmiro Cacoma <edmirocacoma99@gmail.com>
2024-11-21 13:45:49 +0100

    login function added

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,12 @@ export default async function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center bg-sky-400">
       <div className="space-y-6 text-center">
-        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md", font.className)}>ꗃ AUTH</h1>
-        <p className="text-white text-lg">Authentication Service</p>
+        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md", font.className)}>ꗃ BIVA </h1>
+        <p className="text-white text-lg">Faça login ou registe-se e conheça um <strong>mundo novo</strong> </p>
       </div>
       <div>
         <LoginButton>
-          <Button variant={'secondary'} className='w-full' size={'lg'}>sign in</Button>
+          <Button variant={'secondary'} className='w-full mt-6' size={'lg'}>Começar</Button>
         </LoginButton>
         
       </div>
---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import authConfig from "@/auth.config";
+import NextAuth from "next-auth";
+import { DEFAULT_LOGIN_REDIRECT, authRoutes, apiAuthPrefix, publicRoutes } from '@/routes';
+
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth;
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+    if (isApiAuthRoute) {
+        return undefined
+    }
+
+    if (isAuthRoute) {
+        if (isLoggedIn) {
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+        }
+       return undefined
+    }
+
+    if (!isLoggedIn && !isPublicRoute) {
+        return Response.redirect(new URL("/auth/login", nextUrl))
+    }
+
+    return undefined
+})
